af1b240d1c4014f51767b11a451b49157bd4a08d
jszyman <jszyman@users.noreply.github.com>
2019-01-08 12:34:04 +0100

    added StopWatch state machine example, no observable output yet

--- boost_demo/boost_statechart/boost_statechart.cpp
+++ boost_demo/boost_statechart/boost_statechart.cpp
@@ -1,6 +1,7 @@
 // boost_statechart.cpp : Defines the entry point for the console application.
 //
 
+#include <boost/statechart/event.hpp>
 #include <boost/statechart/state_machine.hpp>
 #include <boost/statechart/simple_state.hpp>
 #include <iostream>
@@ -46,6 +47,40 @@ struct Greeting : sc::simple_state< Greeting, Machine >
     ~Greeting() { std::cout << "Bye Bye World!\n"; } // exit
 };
 
+struct EvStartStop : sc::event< EvStartStop > {};
+struct EvReset : sc::event< EvReset > {};
+
+struct Active;
+struct StopWatch : sc::state_machine< StopWatch, Active > {};
+
+struct Stopped;
+
+// The simple_state class template accepts up to four parameters:
+// - The third parameter specifies the inner initial state, if
+//   there is one. Here, only Active has inner states, which is
+//   why it needs to pass its inner initial state Stopped to its
+//   base
+// - The fourth parameter specifies whether and what kind of
+//   history is kept
+
+// Active is the outermost state and therefore needs to pass the
+// state machine class it belongs to
+struct Active : sc::simple_state<
+    Active, StopWatch, Stopped > {};
+
+// Stopped and Running both specify Active as their Context,
+// which makes them nested inside Active
+struct Running : sc::simple_state< Running, Active > {};
+struct Stopped : sc::simple_state< Stopped, Active > {};
+
+// Because the context of a state must be a complete type (i.e.
+// not forward declared), a machine must be defined from
+// "outside to inside". That is, we always start with the state
+// machine, followed by outermost states, followed by the direct
+// inner states of outermost states and so on. We can do so in a
+// breadth-first or depth-first way or employ a mixture of the
+// two.
+
 int main()
 {
     Machine myMachine;
@@ -55,5 +90,10 @@ int main()
     myMachine.initiate();
     // When we leave main(), myMachine is destructed what leads to
     // the destruction of all currently active states.
+
+    StopWatch myWatch;
+    myWatch.initiate();
+
+
     return 0;
 }
